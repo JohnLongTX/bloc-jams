6439bb67eaecf8ecf22e8e29e4086426db940110
John Long <johnlong73801@gmail.com>
2018-03-16 17:57:53 -0500

    Checkpoint 7 blocjams audio playback completed

--- src/components/Album.js
+++ src/components/Album.js
@@ -10,9 +10,44 @@ class Album extends Component {
     });
 
     this.state = {
-      album: album
+      album: album,
+      currentSong: album.songs[0],
+      isPlaying: false
     };
+
+    this.audioElement = document.createElement('audio');
+    this.audioElement.src = album.songs[0].audioSrc;
   }
+
+  play() {
+    this.audioElement.play();
+    this.setState({ isPlaying: true });
+  }
+
+  pause() {
+    this.audioElement.pause();
+    this.setState({ isPlaying: false });
+  }
+
+  setSong(song) {
+    this.audioElement.src = song.audioSrc;
+    this.setState({ currentSong: song });
+  }
+
+  handleSongClick(song) {
+    const isSameSong = this.state.currentSong === song;
+    console.log(isSameSong);
+    console.log(this.state.isPlaying);
+    if (this.state.isPlaying && isSameSong) {
+      console.log('pause');
+      this.pause();
+    }else {
+      console.log('play');
+      if (!isSameSong) { this.setSong(song); } 
+      this.play();
+    }
+  }
+
   render() {
     return (
       <section className="album">
@@ -38,7 +73,7 @@ class Album extends Component {
             </tr>
             {
               this.state.album.songs.map( (song, index) =>
-                <tr className="song" key={index}>
+                <tr className="song" key={index} onClick={ () => this.handleSongClick(song) }>
                   <td className="song-number">{index + 1}</td>
                   <td className="song-title">{song.title}</td>
                   <td className="song-duration">{song.duration}</td>
